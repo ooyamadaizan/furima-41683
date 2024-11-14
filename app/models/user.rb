36e67938e8a319f8201580076ef1e8d5d76028f3
ooyamadaizan <daizandeisia@gmail.com>
2024-11-14 15:21:57 +0900

    userモデルのバリデーションを追加
    正規表現と属性の値がマッチできるように作成しました。
    passwordには半角英数混合を含むことを設定
    first_name last_nameには全角（漢字・ひらがな・カタカナ）を
    first_name_kanaとlast_name_kana
    には全角（カタカナ）含むことを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
          validates :first_name_kana, presence: true
          validates :last_name_kana, presence: true
          validates :birthday, presence: true
+         validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])/ , message: "は半角英数字を含めてください" }
+         validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+         validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+         validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "は全角（カタカナ）で入力してください" }
+         validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "は全角（カタカナ）で入力してください" }
 end
