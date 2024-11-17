fe8d6370e740fb88c5f423153c284be97b31a10d
ooyamadaizan <daizandeisia@gmail.com>
2024-11-17 20:16:45 +0900

    商品出品のアクティブハッシュ実装
    バリデーションで"―"が選ばれないように設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
-         validates :birthday, presence: true
-         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: "is invalid. Include both letters and numbers" }
-         validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
-         validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
-         validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
-         validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: "is invalid. Include both letters and numbers" }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
+  
+  has_many :items
 end
