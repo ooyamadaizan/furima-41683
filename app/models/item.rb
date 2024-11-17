2eab6cb64f6daa51a51aeea0e7d679ed5996b080
ooyamadaizan <daizandeisia@gmail.com>
2024-11-18 04:31:10 +0900

    バリデーションの追加
    imageとpriceの記述を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,18 +8,13 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   has_one_attached :image
 
+  validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
-
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }, format: { with: /\A[0-9]+\z/, message: "は半角数字のみで入力してください" }
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 end
