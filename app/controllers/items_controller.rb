6521cb574dab75b7739caac32aba7c92e4356ce8
ooyamadaizan <daizandeisia@gmail.com>
2024-11-17 20:18:40 +0900

    商品出品機能のcreateアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
-  
+
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
 end
+
