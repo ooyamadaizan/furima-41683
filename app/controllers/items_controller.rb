a49d314c6a7d35855d7201736142cdd0cb4a48f4
ooyamadaizan <daizandeisia@gmail.com>
2024-11-20 17:03:33 +0900

    ログイン中でも他人が出品した商品の編集ページには遷移できず、トップページにリダイレクトされるように設定実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,8 +41,12 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
 end
