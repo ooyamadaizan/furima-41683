f822fbe3357c946d4544de551ac5f5878c4e1011
ooyamadaizan <daizandeisia@gmail.com>
2024-11-21 15:02:15 +0900

    購入機能とのAssociationを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
   has_one_attached :image
+  has_one :order
 
   validates :image, presence: true
   validates :name, presence: true
