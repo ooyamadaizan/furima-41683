257137a548ca51aa1baa7f17d47a55e62cacd4ed
ooyamadaizan <daizandeisia@gmail.com>
2024-11-21 21:15:45 +0900

    createの設定を実施
    （order_addresses_formの名前を変更したのでいろいろと変わってます。）
    order_address_formに変更。
    ストロングパラメータ―設定
    indexのviewも設定済み

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,25 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @order = OrderAddressesForm.new
+    @order_address_form = OrderAddressForm.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_address_form = OrderAddressForm.new(order_address_form_params)
+    binding.pry
+    if @order_address_form.valid?
+      @order_address_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def order_address_form_params
+    params.require(:order_address_form).permit(:postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number, ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
 end
---
+++ app/models/order_address_form.rb
@@ -0,0 +1,31 @@
+class OrderAddressForm
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number, :order_id, :token
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address_line
+    validates :phone_number
+    validates :token
+  end
+ 
+  
+  def save
+
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      address_line: address_line,
+      building_name: building_name,
+      phone_number: phone_number,
+      order_id: order.id
+    )
+  end
+end
