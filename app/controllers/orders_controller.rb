467c6700a7a5309cfbebd7ce1bf1eb76a2de3f10
ooyamadaizan <daizandeisia@gmail.com>
2024-11-25 17:30:12 +0900

    ログイン状態のみ操作できるように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
