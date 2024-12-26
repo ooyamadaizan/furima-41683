fe8002572c88ceb69573a618e02078126ee9e2bf
ooyamadaizan <daizandeisia@gmail.com>
2024-12-26 21:24:40 +0900

    動的に画像プレビュー機能を設定
    画像ファイル添付時に表示用のdiv要素とimg要素を生成しdiv要素にクラスをimg要素にクラスとURLを設定し
    img要素を生成したdiv要素の中に入れてページに設定しているidのpreviewsに設定することで表示されるようになりました。
    それとif文を使って生成済みの画像があればそれを削除し新しい画像を表示できるようにしました。

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,25 @@
 document.addEventListener('turbo:load', function(){
   const previewList = document.getElementById('previews');
   if(!previewList) return null;
-  console.log("preview.jsが読み込まれました");
+
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  fileField.addEventListener('change', function(e){
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview){
+      alreadyPreview.remove();
+    };
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const previewWrapper = document.createElement('div')
+    previewWrapper.setAttribute('class', 'preview');
+
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'img-previews');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
 });
