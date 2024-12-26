85a905de89614ce1eb149655852fe9579dc71db4
ooyamadaizan <daizandeisia@gmail.com>
2024-12-26 19:22:58 +0900

    商品出品ページにてプレビュー画像を表示する場所を作成
    javascriptを読み込まれるようにpostForm名をpreviewListに変更
    previewListの読み込みクラス名を変更
    動的に表示する画像のcssを記述
    new.html.erbにpreviewsのidを追加。image-fieldのクラスを作成し作成したidとweight-bold-textクラスを子にしました。

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,5 @@
 document.addEventListener('turbo:load', function(){
-  const postForm = document.getElementById('new_post');
-  if(!postForm) return null;
+  const previewList = document.getElementById('previews');
+  if(!previewList) return null;
   console.log("preview.jsが読み込まれました");
 });
